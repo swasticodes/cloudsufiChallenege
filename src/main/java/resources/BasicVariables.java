3a4185b4ebb67ca6605812a2bb4a56e305b666f1
Roman Himmes <Roman.Himmes@inform-software.com>
2019-01-25 16:52:39 +0100

    Added remote property to connect to a remote web driver

--- src/main/java/resources/BasicVariables.java
+++ src/main/java/resources/BasicVariables.java
@@ -3,6 +3,8 @@ package resources;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -12,47 +14,96 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasicVariables {
 	public static WebDriver driver;
 	public Properties prop;
 	public WebDriver initializeDriver() throws IOException
 	{
-		 prop= new Properties();
-		 FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\data.properties");
-		 //Load the properties file
-		 prop.load(fis);
-		 String browserName=prop.getProperty("browser");
-		 System.out.println("The browser used for the test is "+browserName);
-		 String URL = prop.getProperty("URL");
-		 if(browserName.equals("chrome"))
-		 {
-			 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\chromedriver.exe");
-			 //System.setProperty("webdriver.chrome.driver", System.getProperty("C:\\Users\\Comp-7\\Desktop\\chromedriver.exe"));
-		 	 driver = new ChromeDriver();
-		 	 //execute in chrome driver
-		 }
-		 else if (browserName.equals("firefox"))
-		 {
+		prop= new Properties();
+		FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\data.properties");
+		//Load the properties file
+		prop.load(fis);
+	
+		String remoteHost = prop.getProperty("remote");
+		if (remoteHost != null) {
+			initializeRemoteWebDriver(remoteHost);
+		}
+		else
+		{
+			initializeDirectWebDriver();
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	public WebDriver initializeRemoteWebDriver(String remoteHost) throws IOException
+	{
+		String browserName=prop.getProperty("browser");
+		DesiredCapabilities capabilities = null;
+		if(browserName.equals("chrome"))
+		{
+			capabilities = DesiredCapabilities.chrome();
+		}
+		else if (browserName.equals("firefox"))
+		{
+			capabilities = DesiredCapabilities.firefox();
+		}
+		else if (browserName.equals("IE"))
+		{
+			capabilities = DesiredCapabilities.internetExplorer();
+		}
+		else if (browserName.equals("edge"))
+		{
+			capabilities = DesiredCapabilities.edge();
+		}
+		else
+		{
+			//Print Message that invalid driver is selected
+			System.out.println("Invalid Browser Selected");
+		}
+		
+		try {
+			driver = new RemoteWebDriver(new URL(remoteHost), capabilities);
+		} catch(MalformedURLException ex) {
+			throw new IOException("Could not open remote host");
+		}
+		return driver;
+	}
+
+	public WebDriver initializeDirectWebDriver() throws IOException
+	{
+		String browserName=prop.getProperty("browser");
+		System.out.println("The browser used for the test is "+browserName);
+		if(browserName.equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", System.getProperty("C:\\Users\\Comp-7\\Desktop\\chromedriver.exe"));
+			driver = new ChromeDriver();
+			//execute in chrome driver
+		}
+		else if (browserName.equals("firefox"))
+		{
 			 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\geckodriver.exe");
-		 	 driver = new FirefoxDriver();
-		 	//execute in firefox browser
-		 }
-		 else if (browserName.equals("IE"))
-		 {
+			 driver = new FirefoxDriver();
+			//execute in firefox browser
+		}
+		else if (browserName.equals("IE"))
+		{
 			 //IE code
-		 }
-		 else
-		 {
+		}
+		else
+		{
 			 //Print Message that invalid driver is selected
 			 System.out.println("Invalid Browser Selected");
-		 }
-		 
-		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		 driver.manage().window().maximize();
-		 return driver;	
+		}
+		
+		return driver;	
 	}
-	
+
 	public void takeScreenshot(String result) throws IOException
 	{
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
