c5743bec13c1dac37e1e39f0657b42cd24ee4a77
swasti <swasti@testtailor.in>
2019-02-19 15:48:54 +0530

    Adding Gidts sets page file

---
+++ src/main/java/pageObjects/GiftsSetsPage.java
@@ -0,0 +1,31 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GiftsSetsPage
+{
+	public WebDriver driver;
+
+	@FindBy (xpath="//*[contains(@class,'container-list')]/div[3]/div[3]/div/div[1]/div[2]/div[2]") WebElement link_SecondItem;
+	//Comment the above code and uncomment the code below in case of using Page Object model instead of Page Factory Model
+	//By link_Login = By.linkText("Anmelden");
+
+	public GiftsSetsPage(WebDriver driver)
+	{
+		this.driver = driver;
+		//Comment the code below in case using Page object instead of Page Factory
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement linkGiftsSetsSecondItem()
+	{
+		//Use the code below in case of using Page Factory Model
+		return link_SecondItem;
+		//Comment the above code and Uncomment the code below in case using Page object instead of Page factory
+		//return driver.findElement(link_Login);
+	}
+}
