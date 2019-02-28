3583deedc98458b8bd293a86552c705b65ea908d
swasti <swasti@testtailor.in>
2019-02-28 14:28:19 +0530

    removed unused imports

--- src/test/java/Blume2000/accountTypeGuestCustomer.java
+++ src/test/java/Blume2000/accountTypeGuestCustomer.java
@@ -1,20 +1,16 @@
 package Blume2000;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import pageObjects.HomePage;
-import pageObjects.LoginPage;
 import pageObjects.OrderOverviewPage;
 import pageObjects.PayPalPage;
 import pageObjects.RegistrationPage;
