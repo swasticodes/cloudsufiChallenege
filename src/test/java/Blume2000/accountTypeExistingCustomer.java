6ef89f5ac197d93a97d7ebf880836c7655719ef1
swasti <swasti@testtailor.in>
2019-03-06 12:41:45 +0530

    modified test case existingCustomerPaypalMethodTopsellerTest for mobile and adjusted the flow

--- src/test/java/Blume2000/accountTypeExistingCustomer.java
+++ src/test/java/Blume2000/accountTypeExistingCustomer.java
@@ -616,7 +616,12 @@ public class accountTypeExistingCustomer<inherits> extends BasicVariables {
 		AddressAndPaymentMethodPage addressAndPaymentMethodPage = new AddressAndPaymentMethodPage(driver);
 
 		ensurePageLoaded();
+		generalPage.clickCloseCookieMessage(false);
 
+		String browserName=prop.getProperty("browser");
+		if(browserName.equalsIgnoreCase("mobile")) {
+			homePage.mobileHamburgerMenu().click();
+		}
 		homePage.linkTopseller().click();
 		log.info("Clicked on the Topseller link");
 		generalPage.linkSecondItem().click();
@@ -626,34 +631,50 @@ public class accountTypeExistingCustomer<inherits> extends BasicVariables {
 		Thread.sleep(1000);
 		deliveryPage.textFieldDeliveryPostalCode().sendKeys("22297");
 		log.info("Entered the delivery postal code 22297");
-		deliveryPage.buttonNextPostalCode().click();
-		log.info("Clicked the next button after entering Postal Code");
+		if(!browserName.equalsIgnoreCase("mobile")) {
+			deliveryPage.buttonNextPostalCode().click();
+			log.info("Clicked the next button after entering Postal Code");
+		}
+		Thread.sleep(1000);
 		deliveryPage.dayActiveDay().click();
 		log.info("Selecting the active day for delivery");
 		Thread.sleep(1000);
 		deliveryPage.continueToGreetingCard().click();
 		log.info("Clicked on Weiter zur Grußkarte button");
 		Thread.sleep(1000);
-		greetingCardPage.tabLove().click();
-		log.info("Clicked on the tab Liebe");
-		greetingCardPage.secondGreetingCardLoveTab().click();
-		log.info("Selected the second Greeting Card on the Love tab");
-		greetingCardPage.textboxGreetingCardText().sendKeys("test message");
-		log.info("Entered the greeting card text");
+		greetingCardPage.linkGreetingCardFirstItem().click();
+		log.info("Selected the first greeting card");
+		if(browserName.equalsIgnoreCase("mobile")) {
+			greetingCardPage.mobileButtonEditGreetingText().click();
+			log.info("Clicked on Gruß bearbeiten button for enetring text");
+		}
+		Thread.sleep(1000);
+		if(browserName.equalsIgnoreCase("mobile")) {
+			greetingCardPage.mobileDropdownSelectGreetingTextTemplate().click();
+		}else {
+			greetingCardPage.dropdownSelectGreetingTextTemplate().click();
+		}
+		log.info("Opened the Grußtextvorlage auswählen dropdown");
+		Thread.sleep(1000);
+		if(browserName.equalsIgnoreCase("mobile")) {
+			greetingCardPage.mobileGreetingCardTemplateLove().click();
+		}else {
+			greetingCardPage.greetingCardTemplateLove().click();
+		}
+		log.info("Selectd Liebe from the dropdown");
+		Thread.sleep(1000);
 		greetingCardPage.buttonContinueToGifts().click();
 		log.info("Clicked on the button 'Weiter zu Geschenke'");
 		giftsPage.linkGiftsPageFirstItem().click();
 		log.info("Selected the first Gift Item");
-		giftsPage.linkGiftsPageSecondItem().click();
-		log.info("Selected the second Gift Item");
 		giftsPage.buttonDirectlyToCashRegister().click();
 		log.info("Clicked on the button 'Direkt zur Kasse'");
-		registerationPage.buttonOrderAsGuest().click();
-		log.info("Clicked on the button 'Als Gast bestellen'");
-		//Creating a random email id to register user
-		String emailId = RandomStringUtils.randomAlphabetic(8);
-		registerationPage.registrationEmail().sendKeys(emailId+"@testemail.com");
-		log.info("For registeration entered email id "+ emailId);
+		registerationPage.textFieldEmail().sendKeys(registerationPage.registeredUserEmail());
+		log.info("Entered the email id as-> " + registerationPage.registeredUserEmail());
+		registerationPage.textFieldPassword().sendKeys(registerationPage.registeredUserPassword());
+		log.info("Entered the password as-> <CENSORED>");
+		registerationPage.buttonLogin().click();
+		log.info("Clicked on the 'Einloggen' button");
 		registerationPage.registrationSalutation().click();
 		log.info("For registration selected salutation as Herr");
 		registerationPage.registrationFirstName().sendKeys("TestFirst");
@@ -666,9 +687,6 @@ public class accountTypeExistingCustomer<inherits> extends BasicVariables {
 		log.info("For registeration entered street number as 33");
 		registerationPage.registrationCity().sendKeys("Hamburg");
 		log.info("For registeration entered city as Hamburg");
-		//((JavascriptExecutor) driver).executeScript("window.scrollBy(0,-180)");
-		registerationPage.copyDeliveryAndInvoiceAddress().click();
-		log.info("Checking the checkbox so that delivery and invoice address are same");
 		addressAndPaymentMethodPage.radioButtonPayPal().click();
 		log.info("Clicking PayPal radio button");
 		registerationPage.continueToOverview().click();
@@ -682,7 +700,11 @@ public class accountTypeExistingCustomer<inherits> extends BasicVariables {
 		payPalPage.buttonPaypalPaymentConfirmation().click();
 		driver.switchTo().defaultContent();
 		log.info("Clicked on the 'Jetzt bezahlen' button for payment confirmation");
+		if(browserName.equalsIgnoreCase("mobile")) {
+			Assert.assertTrue(generalPage.textOrderConfirmation().getText().contains("Glückwunsch"));
+		}else {
 		Assert.assertEquals("Glückwunsch! Gute Wahl getroffen", generalPage.textOrderConfirmation().getText());
+		}
 		log.info("Order is placed successfully");
 
 		log.info("*** Finished Test: existingCustomerPaypalMethodTopsellerTest");
