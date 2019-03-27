949c3c94efb73da8acc5679e69dac36bf4fe204f
swasti <swasti@testtailor.in>
2019-03-27 14:23:22 +0530

    added missing import for select

--- src/test/java/Blume2000/accountTypeGuestCustomer.java
+++ src/test/java/Blume2000/accountTypeGuestCustomer.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
