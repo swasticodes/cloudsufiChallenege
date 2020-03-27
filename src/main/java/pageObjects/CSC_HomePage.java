aa0e9e5ab26842fed1aa56dc3c7f6de8ce69fe47
Mehta <amehta@private.blume2000.de>
2020-03-27 11:40:48 +0100

    Adding PageObject Files for CSC tool.

---
+++ src/main/java/pageObjects/CSC_HomePage.java
@@ -0,0 +1,33 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import resources.BasicVariables;
+
+public class CSC_HomePage
+{
+	public WebDriver driver;
+	BasicVariables bv = new BasicVariables();
+
+	@FindBy (xpath="//*[text() ='Neu']") WebElement btn_NewOrder;
+	@FindBy (xpath="(//*[contains(@class,'v-menubar-menuitem v-menubar-menuitem-primary')])[2]") WebElement btn_NewCustomer;
+	//Comment the above code and uncomment the code below in case of using Page Object model instead of Page Factory Model
+	//By link_Login = By.linkText("Anmelden");
+
+	public CSC_HomePage(WebDriver driver)
+	{
+		this.driver = driver;
+		//Comment the code below in case using Page object instead of Page Factory
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement buttonNewCustomer()
+	{
+		//Use the code below in case of using Page Factory Model
+		return btn_NewCustomer;
+		//Comment the above code and Uncomment the code below in case using Page object instead of Page factory
+		//return driver.findElement(link_Login);
+	}
+}
